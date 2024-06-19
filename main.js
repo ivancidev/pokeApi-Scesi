f3fb31c06e11c116752d69ce7cabe6d99fcaf80d
Herlan <herlanherbas@gmail.com>
2024-06-18 23:13:28 -0400

    create searchPokemon mainjs

---
+++ main.js
@@ -0,0 +1,32 @@
+const URL = "https://pokeapi.co/api/v2/pokemon/";
+
+const valueInput = document.getElementById("search");
+const button = document.querySelector("button");
+const pokedex = document.getElementById("pokedex");
+const btnSearch = document.getElementById("btnSearch");
+
+btnSearch.addEventListener("click", searchPokemon);
+
+async function searchPokemon() {
+  try {
+    const response = await fetch(URL + valueInput.value.toLowerCase());
+    if (!response.ok) {
+      showError();
+    }
+
+    const data = await response.json();
+    pokedex.innerHTML = `
+    <h2>${data.name.toUpperCase()}</h2>
+    <img src="${data.sprites.front_default}" alt="${data.name}"/>
+    <p>Altura: ${data.height}</p>
+    <p>Peso: ${data.weight}</p>
+    
+    `;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function showError() {
+  pokedex.innerHTML = `<h2 class="error">Pokemon no encontrado</h2>`;
+}
